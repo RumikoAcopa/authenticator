6bc7b96a045c33a39bf6157dcfbdcb9cff8159dd
rumikoacopa <rumikorios@gmail.com>
2021-06-27 22:42:53 -0400

    create an array with hash objects that contain k,v

---
+++ playground.rb
@@ -0,0 +1,7 @@
+users = [
+  { username: 'Miranda', password: 'password1' },
+    { username: 'Scott', password: 'password2' },
+      { username: 'Angela', password: 'password3' },
+        { username: 'Flora', password: 'password4' },
+          { username: 'Juan', password: 'password5' }
+]
